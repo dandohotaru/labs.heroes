d1e08bc46f03f9b4fab6c8f6c3074ef24dd5d826
Dohotar Dan <ddoh@cor.europa.eu>
2016-11-30 09:48:55 +0100

    load partials inside another template

--- katas/05-handlebars-demo/src/index.js
+++ katas/05-handlebars-demo/src/index.js
@@ -17,39 +17,46 @@
 //     load('persons.json', '#template', '#content'); 
 // });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  //Get template from server
-  $.get("./app/header.hbs").done(function(response) {
-    var content = $($.parseHTML(response));
+  var mainLoader = $.get("./app/main.hbs");
+  var headerLoader = $.get("./app/shared/header.hbs");
+  var footerLoader = $.get("./app/shared/footer.hbs");
 
-    //Compile main template
-    var template = Handlebars.compile(content.filter("#test-template").html());
+  $.when(mainLoader, headerLoader, footerLoader)
+    .done(function (mainResult, headerResult, footerResult) {
 
-    //You need to register each partial
-    Handlebars.registerPartial({
-      foo: content.filter("#foo-partial").html(),
-      bar: content.filter("#bar-partial").html()
+      var mainHtml = $($.parseHTML(mainResult[0])).filter("#main-partial").html();
+      var headerHtml = $($.parseHTML(headerResult[0])).filter("#header-partial").html();
+      var footerHtml = $($.parseHTML(footerResult[0])).filter("#footer-partial").html();
+
+      var template = Handlebars.compile(mainHtml);
+
+      Handlebars.registerPartial({
+        foo: headerHtml,
+        bar: footerHtml
+      });
+
+      var data = {
+        "test": [{
+          foo: "Foo",
+          "foo_bar": "Foo-Bar",
+          bar: "Bar",
+          bar_foo: "Bar-Foo"
+        }, {
+          foo: "Foo2",
+          "foo_bar": "Foo-Bar2",
+          bar: "Bar2",
+          bar_foo: "Bar-Foo2"
+        }]
+      };
+
+      document.body.innerHTML = template(data);
+
+    }).fail(function (error) {
+      console.error(error);
     });
 
-    //Your data
-    var data = {
-      "test": [{
-        foo: "Foo",
-        "foo_bar": "Foo-Bar",
-        bar: "Bar",
-        bar_foo: "Bar-Foo"
-      }, {
-        foo: "Foo2",
-        "foo_bar": "Foo-Bar2",
-        bar: "Bar2",
-        bar_foo: "Bar-Foo2"
-      }]
-    };
-
-    document.body.innerHTML = template(data);
-
-  });
 });
 
 
