61cfc91d875e4153d07688ea3bc2a73a8f14b90e
Dohotar Dan <ddoh@cor.europa.eu>
2016-11-30 15:35:51 +0100

    add menu details with themes and commissions

--- katas/05-handlebars-demo/src/app/opinions.js
+++ katas/05-handlebars-demo/src/app/opinions.js
@@ -1,35 +1,52 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-  var headerLoader = $.get("/app/shared/header.hbs");
-  var footerLoader = $.get("/app/shared/footer.hbs");
-  var notificationsLoader = $.getJSON("/api/notificatios.json");
+    var headerLoader = $.get("/app/shared/header.hbs");
+    var menuLoader = $.get("/app/shared/menu.hbs");
+    var footerLoader = $.get("/app/shared/footer.hbs");
+    var notificationsLoader = $.getJSON("/api/notificatios.json");
+    var themesLoader = $.getJSON("/api/themes.json");
 
-  $.when(headerLoader, footerLoader, notificationsLoader)
-    .done(function (headerResult, footerResult, notificationsResult) {
+    $.when(headerLoader, menuLoader, footerLoader, notificationsLoader, themesLoader)
+        .done(function(headerResult, menuResult, footerResult, notificationsResult, themesResult) {
 
-      // Header
-      var notificationsData = notificationsResult[0];
-      var headerPartial = $($.parseHTML(headerResult[0])).filter("#header-partial").html();
-      var headerTemplate = Handlebars.compile(headerPartial);
-      var headerMarkup = headerTemplate({
-        user: {
-          firstName: "John",
-          lastName: "Doe"
-        },
-        notifications: notificationsData
-      });
-      $("#header").html(headerMarkup);
+            // Header
+            var notificationsData = notificationsResult[0];
+            var headerPartial = $($.parseHTML(headerResult[0])).filter("#header-partial").html();
+            var headerTemplate = Handlebars.compile(headerPartial);
+            var headerMarkup = headerTemplate({
+                user: {
+                    firstName: "John",
+                    lastName: "Doe"
+                },
+                notifications: notificationsData
+            });
+            $("#header").html(headerMarkup);
 
-      // Footer
-      var footerHtml = $($.parseHTML(footerResult[0])).filter("#footer-partial").html();
-      var footerTemplate = Handlebars.compile(footerHtml);
-      $("#footer").html(footerTemplate({
-        lastUpdate: "30th of November 2016"
-      }));
+            // Menu
+            var themesData = _.map(themesResult[0].themes, function(p) {
+                return {
+                    id: p.id,
+                    name: p.name,
+                    counter: p.subthemes.length,
+                    url: "opinions/cards.html?themeId=" + p.id
+                };
+            });
+            var menuHtml = $($.parseHTML(menuResult[0])).filter("#menu-partial").html();
+            var menuTemplate = Handlebars.compile(menuHtml);
+            $("#menu").html(menuTemplate({
+                themes: themesData
+            }));
 
-    }).fail(function (error) {
-      console.error(error);
-    });
+            // Footer
+            var footerHtml = $($.parseHTML(footerResult[0])).filter("#footer-partial").html();
+            var footerTemplate = Handlebars.compile(footerHtml);
+            $("#footer").html(footerTemplate({
+                lastUpdate: "30th of November 2016"
+            }));
+
+        }).fail(function(error) {
+            console.error(error);
+        });
 
 });
 
--- katas/05-handlebars-demo/src/index.js
+++ katas/05-handlebars-demo/src/index.js
@@ -1,37 +1,62 @@
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-  var headerLoader = $.get("/app/shared/header.hbs");
-  var footerLoader = $.get("/app/shared/footer.hbs");
-  var notificationsLoader = $.getJSON("/api/notificatios.json");
+    var headerLoader = $.get("/app/shared/header.hbs");
+    var menuLoader = $.get("/app/shared/menu.hbs");
+    var footerLoader = $.get("/app/shared/footer.hbs");
+    var notificationsLoader = $.getJSON("/api/notificatios.json");
+    var themesLoader = $.getJSON("/api/themes.json");
+    var commissionsLoader = $.getJSON("/api/commissions.json");
 
-  $.when(headerLoader, footerLoader, notificationsLoader)
-    .done(function (headerResult, footerResult, notificationsResult) {
+    $.when(headerLoader, menuLoader, footerLoader, notificationsLoader, themesLoader, commissionsLoader)
+        .done(function(headerResult, menuResult, footerResult, notificationsResult, themesResult, commissionsResult) {
 
-      // Header
-      var notificationsData = notificationsResult[0];
-      var headerPartial = $($.parseHTML(headerResult[0])).filter("#header-partial").html();
-      var headerTemplate = Handlebars.compile(headerPartial);
-      var headerMarkup = headerTemplate({
-        user: {
-          firstName: "John",
-          lastName: "Doe"
-        },
-        notifications: notificationsData
-      });
-      $("#header").html(headerMarkup);
+            // Header
+            var notificationsData = notificationsResult[0];
+            var headerPartial = $($.parseHTML(headerResult[0])).filter("#header-partial").html();
+            var headerTemplate = Handlebars.compile(headerPartial);
+            var headerMarkup = headerTemplate({
+                user: {
+                    firstName: "John",
+                    lastName: "Doe"
+                },
+                notifications: notificationsData
+            });
+            $("#header").html(headerMarkup);
 
-      // Footer
-      var footerHtml = $($.parseHTML(footerResult[0])).filter("#footer-partial").html();
-      var footerTemplate = Handlebars.compile(footerHtml);
-      $("#footer").html(footerTemplate({
-        lastUpdate: "30th of November 2016"
-      }));
-      
+            // Menu
+            var themesData = _.map(themesResult[0].themes, function(p) {
+                return {
+                    id: p.id,
+                    name: p.name,
+                    counter: p.subthemes.length,
+                    url: "browse/cards.html?themeId=" + p.id
+                };
+            });
+            var commissionsData = _.map(commissionsResult[0], function(p) {
+                return {
+                    name: p.name,
+                    counter: Math.floor((Math.random() * 100) + 1),
+                    url: "browse/cards.html?commission=" + p.name
+                };
+            });
+            var menuHtml = $($.parseHTML(menuResult[0])).filter("#menu-partial").html();
+            var menuTemplate = Handlebars.compile(menuHtml);
+            $("#menu").html(menuTemplate({
+                themes: themesData,
+                commissions: commissionsData,
+            }));
 
-    }).fail(function (error) {
-      console.error(error);
-    });
+            // Footer
+            var footerHtml = $($.parseHTML(footerResult[0])).filter("#footer-partial").html();
+            var footerTemplate = Handlebars.compile(footerHtml);
+            $("#footer").html(footerTemplate({
+                lastUpdate: "30th of November 2016"
+            }));
+
+        }).fail(function(error) {
+            console.error(error);
+        });
 });
 
 
